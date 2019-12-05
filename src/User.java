4c540fcfc47d703c47982def3e03672bcdb8004e
DuncanRodman <55167198+DuncanRodman@users.noreply.github.com>
2019-12-05 07:05:34 -0600

    Add files via upload

--- src/User.java
+++ src/User.java
@@ -8,50 +8,57 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class User {
-	//public static void main (String args []) {
-//		try {
-//			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-//			String path = System.getProperty("user.dir")+"\\"+"ToDoDB.accdb";
-//			String url = "jdbc:ucanaccess://"+path;
-//			Connection con = DriverManager.getConnection(url);
-//			System.out.println("Connection successful");
-//				try {
-//					Statement st = con.createStatement();
-//					ResultSet rs = st.executeQuery("SELECT * FROM account");
-//					while (rs.next()) {
-//						System.out.print(rs.getString(1));
-//						System.out.println(""+rs.getString(2));
-//					}
-//			}
-//				catch (Exception ex1) {
-//					System.out.println("Unable to locate login credentials");
-//				}
-//			}
-//		catch(Exception ex2) {
-//			System.out.println("Unable to establish database connection");
-//			}
-//	}
-
-	
-
-
-	public static boolean getName (String userName) {
+	public static void dbConnect () {
 		try {
-			boolean check;
 			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
 			String path = System.getProperty("user.dir")+"\\"+"ToDoDB.accdb";
 			String url = "jdbc:ucanaccess://"+path;
 			Connection con = DriverManager.getConnection(url);
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery("SELECT * FROM account WHERE name=?");
-			String sql ="SELECT * from account where name=?";
+			System.out.println("Connection successful");
+				try {
+					Statement st = con.createStatement();
+					ResultSet rs = st.executeQuery("SELECT * FROM account");
+					while (rs.next()) {
+						System.out.print(rs.getInt(1));
+						System.out.print(" "+rs.getString(2));
+						System.out.print(" "+rs.getString(3));
+						System.out.println("");
+					}
+			}
+				catch (Exception ex1) {
+					System.out.println("Unable to locate login credentials");
+				}
+			}
+		catch(Exception ex2) {
+			System.out.println("Unable to establish database connection");
+			}
+	}
+
+	
+
+
+	public boolean getName (String userName) {
+		try {
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+			String path = System.getProperty("user.dir")+"\\"+"ToDoDB.accdb";
+			String url = "jdbc:ucanaccess://"+path;
+			Connection con = DriverManager.getConnection(url);
 			
+			Statement st = con.createStatement();
+			String query="SELECT user_name FROM account WHERE userName='";
+			ResultSet rs = st.executeQuery(query);
+			
+			while(rs.next()) {
+				System.out.println("Name: " + rs.getString(2));
 		}
-		catch (Exception ex3) {};
-		boolean check = false;
-		return check;
+			}
+
+
 		
-		
+		catch (Exception ex3) {
+			System.out.println("ERROR");
+		};
+		return true;
 	}
 	public void getPass (String userPass) {
 
